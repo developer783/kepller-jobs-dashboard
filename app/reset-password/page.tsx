6038713dd1813fa2ed9e89a5a3d79e1da34bb58f
ransd123 <117431830+ransd123@users.noreply.github.com>
2026-02-19 01:35:55 +0530

    Create ResetPasswordPage component

---
+++ app/reset-password/page.tsx
@@ -0,0 +1,8 @@
+export default function ResetPasswordPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Reset Password</h1>
+      <p>This is the reset password page.</p>
+    </div>
+  );
+}
