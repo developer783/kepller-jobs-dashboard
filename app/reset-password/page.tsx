97cd98ca19b420beef4226620645445fed653cd4
annie <anniepriya7777@gmail.com>
2026-03-10 00:03:28 +0530

    updated

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 export default function ResetPasswordPage() {
   return (
     <div style={{ padding: "20px" }}>
@@ -7,14 +7,5 @@ export default function ResetPasswordPage() {
     </div>
   );
 }
-=======
-"use client";
 
-export default function ResetPasswordPage() {
-  return (
-    <div>
-      <h1>Reset Password</h1>
-    </div>
-  );
-}
->>>>>>> 022b123 (Fix actor dataset fetch and normalization)
+
